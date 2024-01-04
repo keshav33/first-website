ea7af77d11f91166bddfc9a6ea070abb1d43ae1b
keshav33 <38713053+keshav33@users.noreply.github.com>
2024-01-04 16:46:07 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,12 @@
 window.onload = function () {
-	$("#header").load("/header.html");
+	$("#header").load("/header.html", () => {
+		const header = document.querySelector("header");
+		const hamburgerBtn = document.querySelector("#hamburger-btn");
+		const closeMenuBtn = document.querySelector("#close-menu-btn");
+		// Toggle mobile menu on hamburger button click
+		hamburgerBtn.addEventListener("click", () => header.classList.toggle("show-mobile-menu"));
+		// Close mobile menu on close button click
+		closeMenuBtn.addEventListener("click", () => hamburgerBtn.click());
+	})
 	$("#footer").load("/footer.html");
-	const header = document.querySelector("header");
-	const hamburgerBtn = document.querySelector("#hamburger-btn");
-	const closeMenuBtn = document.querySelector("#close-menu-btn");
-	// Toggle mobile menu on hamburger button click
-	hamburgerBtn.addEventListener("click", () => header.classList.toggle("show-mobile-menu"));
-	// Close mobile menu on close button click
-	closeMenuBtn.addEventListener("click", () => hamburgerBtn.click());
-	window.addEventListener('scroll', function (e) {
-		if (window.pageYOffset > 100) {
-			document.querySelector("header").classList.add('is-scrolling');
-		} else {
-			document.querySelector("header").classList.remove('is-scrolling');
-		}
-	});
-
-	const menu_btn = document.querySelector('.hamburger');
-	const mobile_menu = document.querySelector('.mobile-nav');
-
-	menu_btn.addEventListener('click', function () {
-		menu_btn.classList.toggle('is-active');
-		mobile_menu.classList.toggle('is-active');
-	});
 }
