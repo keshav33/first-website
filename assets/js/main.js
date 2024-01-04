1f0124710c7a97fdedeb433da1f0db2284466863
keshav33 <38713053+keshav33@users.noreply.github.com>
2024-01-04 15:35:12 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,13 @@
 window.onload = function () {
+	$("#header").load("/header.html");
+	$("#footer").load("/footer.html");
+	const header = document.querySelector("header");
+	const hamburgerBtn = document.querySelector("#hamburger-btn");
+	const closeMenuBtn = document.querySelector("#close-menu-btn");
+	// Toggle mobile menu on hamburger button click
+	hamburgerBtn.addEventListener("click", () => header.classList.toggle("show-mobile-menu"));
+	// Close mobile menu on close button click
+	closeMenuBtn.addEventListener("click", () => hamburgerBtn.click());
 	window.addEventListener('scroll', function (e) {
 		if (window.pageYOffset > 100) {
 			document.querySelector("header").classList.add('is-scrolling');
