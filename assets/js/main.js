1ace93d2ed4d8209673ac003b7f944186784a82a
keshav33 <38713053+keshav33@users.noreply.github.com>
2024-01-13 22:30:46 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,7 @@ window.onload = function () {
 			}
 		})
 		window.addEventListener("hashchange", (event) => {
+			header.classList.remove("show-mobile-menu");
 			if (window.location.href === event.newURL) {
 				document.querySelectorAll(".header a").forEach(link => {
 					if (event.newURL === link.href) {
@@ -43,83 +44,27 @@ window.onload = function () {
 		});
 	});
 
-	class countUp {
-		constructor(el) {
-			this.el = el;
-			this.setVars();
-			this.init();
-		}
-
-		setVars() {
-			this.number = this.el.querySelectorAll("[data-countup-number]");
-			this.observerOptions = { root: null, rootMargin: "0px 0px", threshold: 0 };
-			this.observer = new IntersectionObserver((entries) => {
-				entries.forEach((entry) => {
-					const end = parseFloat(
-						entry.target.dataset.countupNumber.replace(/,/g, "")
-					);
-					const decimals = this.countDecimals(end);
-					if (entry.isIntersecting) {
-						this.iterateValue(entry.target, end, decimals);
-					}
-				});
-			}, this.observerOptions);
-		}
-
-		init() {
-			if (this.number.length > 0) {
-				this.number.forEach((el) => {
-					this.observer.observe(el);
-				});
-			}
-		}
-
-		iterateValue(el, end, decimals) {
-			const start = 0;
-			const duration = 2500;
-			let startTimestamp = null;
-
-			const step = (timestamp) => {
-				if (!startTimestamp) startTimestamp = timestamp;
-				const elapsedPercent = (timestamp - startTimestamp) / duration;
-				const easedProgress = Math.min(this.easeOutQuint(elapsedPercent), 1);
-				let interimNumber = Math.abs(easedProgress * (end - start) + start);
-				el.innerHTML = this.formatNumber(interimNumber, decimals);
-				if (easedProgress < 1) {
-					window.requestAnimationFrame(step);
+	var isAlreadyRun = false;
+	$(window).scroll(function () {
+		$('#why-us-stats').each(function (i) {
+			var bottom_of_object = $(this).position().top + $(this).outerHeight() / 2;
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+			if (bottom_of_window > (bottom_of_object + 20)) {
+				if (!isAlreadyRun) {
+					$('.why-us-counter').each(function () {
+						$(this).prop('Counter', 0).animate({
+							Counter: $(this).text()
+						}, {
+							duration: 3500,
+							easing: 'swing',
+							step: function (now) {
+								$(this).text(Math.ceil(now));
+							}
+						});
+					});
 				}
-			};
-
-			// requestAnimationFrame returns DOMHighResTimeStamp as a callback (used as timestamp)
-			window.requestAnimationFrame(step);
-		}
-
-		easeOutQuad(x) {
-			return 1 - Math.pow(1 - x, 3);
-		}
-
-		easeOutQuint(x) {
-			return 1 - Math.pow(1 - x, 5);
-		}
-
-		countDecimals(val) {
-			if (Math.floor(val) === val) return 0;
-			return val.toString().split(".")[1].length || 0;
-		}
-
-		formatNumber(val, decimals) {
-			return val.toLocaleString("en-US", {
-				minimumFractionDigits: decimals,
-				maximumFractionDigits: decimals
-			});
-		}
-	}
-
-	const dataModules = [...document.querySelectorAll('[data-module="countup"]')];
-
-	dataModules.forEach((element) => {
-		element.dataset.module.split(" ").forEach(function () {
-			new countUp(element);
+				isAlreadyRun = true;
+			}
 		});
 	});
 
